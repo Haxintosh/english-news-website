d1b945636dabc356ed722843aed4ef9d5b2da657
Haxintosh <goose@sillygoosy.ca>
2025-05-10 21:02:08 -0400

    fix: next js base prefix for prod env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? "/english-news-website" : "",
+  assetPrefix: isProd ? "/english-news-website" : "",
+  images: { unoptimized: true }, // GitHub Pages does not support Next.js image optimization
 };
 
 export default nextConfig;
